8f1fc814d501123a3049dfef3ae1ec1d086ce596
unknown <fuaad29@gmail.com>
2024-07-09 19:13:45 +0500

    cmpleted userRouter

--- index.js
+++ index.js
@@ -15,70 +15,6 @@ app.use(express.json());
 // register
 app.use('/', userRouter);
 
-// login
-app.post("/login", async (req, res) => {
-  const user = req.body;
-  // console.log("user.email:", user.email);
-  const editUserArray = await User.find({email: user.email}).exec()
-  const editUser = editUserArray[0]
-  console.log("editUser:", editUser);
-
-  if (editUser.length !== 0 && user.password === editUser.password) {
-      // res.send("User exists and password is correct");
-      // console.log("User exists and password is correct");
-      
-      jwt.sign({ user_id: editUser._id, user_name: editUser.name }, secretKey, (err, token) => {
-        if (err) {
-          res.status(500).send('Error signing token');
-        } else {
-          res.json({ token });
-        }
-      });
-
-  } else if (editUser.length === 0) {
-      // res.send("User doesn't exist");
-      // console.log("User doesn't exist");
-  } else if (editUser.length !== 0 && user.password !== editUser.password) {
-      // res.send("User exists but password is incorrect");
-      // console.log("User exists but password is incorrect");
-  } else {
-      // console.log("none of the above");
-  }
-});
-
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  // console.log("bearerHeader:", bearerHeader);
-  
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ");
-    if (bearer.length === 2 && bearer[0] === "Bearer") {
-      const bearerToken = bearer[1];
-      // console.log("req.token:", req.token);
-      req.token = bearerToken;
-      next();
-    } else {
-      res.sendStatus(403); // Forbidden
-    }
-  } else {
-    res.sendStatus(403); // Forbidden
-  }
-}
-
-// profile
-app.post("/profile", verifyToken, (req, res) => {
-  jwt.verify(req.token, secretKey, (err, authData) => {
-    if (err) {
-      res.send("Token couldn't be verified");
-    } else {
-      res.json({
-        msg: "Token verified successfully",
-        authData
-      });
-    }
-  });
-});
-
 // user posts api
 app.get("/user_posts", verifyToken, async(req, res)=>{
   jwt.verify(req.token, secretKey, async(err, authData) => {
@@ -161,7 +97,6 @@ app.put("/update_post/:_id", async (req, res)=>{
   }
 })
 
-
 // all posts api
 app.get("/all_posts", verifyToken, async(req, res)=>{
   jwt.verify(req.token, secretKey, async(err, authData) => {
@@ -189,5 +124,5 @@ app.get("/users", verifyToken, async(req, res)=>{
 })
 
 app.listen(PORT, () => {
-  // console.log(`server is running at ${PORT}`);
+  console.log(`server is running at ${PORT}`);
 });
