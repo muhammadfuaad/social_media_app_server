3e85792f54e6b95ff2dd0245f4a1f071c8f2f4d2
unknown <fuaad29@gmail.com>
2024-07-11 19:44:44 +0500

    added index.html

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const app = express();
+
 const PORT = 3000;
 const cors = require('cors');
 require("./config");
 const userRouter = require("./routers/userRoute")
-const postRouter = require("./routers/postRoute");
+const postRouter = require("./Routers/postRoute");
 const commentRouter = require("./Routers/commentRoute");
 
-const app = express();
 app.use(cors());
 app.use(express.json());
+var options = {'root': 'C:/Users/Dell/Desktop/Fuaad/coding_practice/projects/social_media_app/server'};
+
+app.get('/', function(req, res){ res.sendFile('/index.html', options);
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+   console.log('A user connected');
+   
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
 
 app.use('/', userRouter);
 app.use('/', postRouter)
